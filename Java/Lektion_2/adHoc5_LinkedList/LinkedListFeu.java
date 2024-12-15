591660582c179a6e9b378ca05b99b1cb5043767a
Denis Korkmaz <deniskorkmaz22@gmail.com>
2024-12-15 22:17:35 +0100

    ad hoc 5

---
+++ Java/Lektion_2/adHoc5_LinkedList/LinkedListFeu.java
@@ -0,0 +1,24 @@
+package Java.Lektion_2.adHoc5_LinkedList;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import DynamicAddressBook.Person;
+
+public class LinkedListFeu {
+
+  private LinkedList<Person> persons = new LinkedList<Person>();
+
+  public void addPerson(Person person) {
+    persons.add(person);
+  }
+
+  public Person getElementAt(int position) {
+    if (position >= 0 && position < persons.size()) {
+      return persons.get(position);
+    } else {
+      throw new NoSuchElementException("Ungültige Position: " + position);
+    }
+  }
+
+}
---
+++ Java/Lektion_2/adHoc5_LinkedList/LinkedListFeuTest.java
@@ -0,0 +1,27 @@
+package Java.Lektion_2.adHoc5_LinkedList;
+
+import java.util.NoSuchElementException;
+
+import DynamicAddressBook.Person;
+
+public class LinkedListFeuTest {
+    public static void main(String[] args) {
+
+        LinkedListFeu list = new LinkedListFeu();
+        list.addPerson(new Person("Sok", 19434491));
+        list.addPerson(new Person("Ali", 19434411));
+        list.addPerson(new Person("Maria", 19444411));
+
+        
+
+         try {
+            System.out.println("Element an Position 1: " + list.getElementAt(0).getName());
+            System.out.println("Element an Position 2: " + list.getElementAt(1).getName());
+            System.out.println("Element an Position 3: " + list.getElementAt(2).getName());
+            System.out.println("Element an Position 9: " + list.getElementAt(9).getName());
+        } catch (NoSuchElementException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
