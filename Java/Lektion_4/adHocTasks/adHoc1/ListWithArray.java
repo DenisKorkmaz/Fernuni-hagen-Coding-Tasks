9f1a77cd0585c7278d3da1e98756642a90a4c7dd
Denis Korkmaz <deniskorkmaz22@gmail.com>
2025-01-19 19:00:02 +0100

    lektion 4 ad-hoc 1

---
+++ Java/Lektion_4/adHocTasks/adHoc1/ListWithArray.java
@@ -0,0 +1,56 @@
+package Java.Lektion_4.adHocTasks.adHoc1;
+
+import java.util.Arrays;
+
+public class ListWithArray<ET> {
+
+    private Object[] internArray;
+    int nextFreeIndex;
+
+    public ListWithArray(int arraySize) {
+        if (arraySize < 1) {
+            arraySize = 1;
+        }
+        internArray = new Object[arraySize];
+    }
+
+    public void resizeArray() {
+        int newSize = internArray.length * 2;
+        if (newSize == 0) {
+            newSize = 1;
+        }
+        internArray = Arrays.copyOf(internArray, newSize);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    ET getLast() {
+        if (nextFreeIndex == 0)
+            throw new java.util.NoSuchElementException();
+        return (ET) internArray[nextFreeIndex - 1];
+    }
+
+    @SuppressWarnings("unchecked")
+    ET removeLast() {
+        if (nextFreeIndex == 0)
+            throw new java.util.NoSuchElementException();
+        ET lastElement = (ET) internArray[nextFreeIndex - 1];
+        nextFreeIndex--;
+        internArray[nextFreeIndex] = null;
+        return lastElement;
+    }
+
+    void addLast(ET element) {
+
+        if (internArray.length <= nextFreeIndex) {
+            resizeArray();
+        }
+        internArray[nextFreeIndex] = element;
+        nextFreeIndex++;
+    }
+
+    int nextFreeIndex() {
+        return nextFreeIndex;
+    }
+
+}
