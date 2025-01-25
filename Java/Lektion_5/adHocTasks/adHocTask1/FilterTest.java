931db3d9e03806adfd52ddc9879b32a7078d3cf1
Denis Korkmaz <deniskorkmaz22@gmail.com>
2025-01-25 22:08:44 +0100

    use lambda function instead of PredicateIsPalindrome

--- Java/Lektion_5/adHocTasks/adHocTask1/FilterTest.java
+++ Java/Lektion_5/adHocTasks/adHocTask1/FilterTest.java
@@ -15,12 +15,22 @@ public class FilterTest {
         Filter<Person> personFilter = new Filter<>(source);
         FilterPredicate<Person> hasShortName = new PredicateShortName();
         FilterPredicate<Person> isLessThanSixtyKg = new PredicateLessThanSixtyKg();
-        FilterPredicate<Person> isPalindrome = new PredicateIsPalindrome();
+        //FilterPredicate<Person> isPalindrome = new PredicateIsPalindrome();
     
 
         List<Person> result = personFilter.filter(hasShortName);
         List<Person> result2 = personFilter.filter(isLessThanSixtyKg);
-        List<Person> result3 = personFilter.filter(isPalindrome);
+       // List<Person> result3 = personFilter.filter(isPalindrome);
+
+       List<Person> isPalindrome = personFilter.filter(
+            p -> {
+                StringBuilder str = new StringBuilder(p.getName());
+                str = str.reverse();
+                String reverseName =  str.toString().toLowerCase();
+                String name = p.getName().toLowerCase();
+                return name.equals(reverseName);
+            }
+        );
 
         for (Person person : result) {
             System.out.println("hasShortName " + person.getName());
@@ -29,7 +39,7 @@ public class FilterTest {
             System.out.println("isLessThanSixtyKg " + person.getName());
             System.out.println(person.getWeight());
         }
-        for (Person person : result3) {
+        for (Person person : isPalindrome) {
             System.out.println("isPalindrome " + person.getName());
         }
     }
