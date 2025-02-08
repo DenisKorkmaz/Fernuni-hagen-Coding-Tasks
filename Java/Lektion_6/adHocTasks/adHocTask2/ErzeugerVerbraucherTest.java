4c21927749b605059c21e3135a5e77d63bbe502c
Denis Korkmaz <deniskorkmaz22@gmail.com>
2025-02-08 20:34:06 +0100

    lektion 6 adHocTask 3

--- Java/Lektion_6/adHocTasks/adHocTask2/ErzeugerVerbraucherTest.java
+++ Java/Lektion_6/adHocTasks/adHocTask2/ErzeugerVerbraucherTest.java
@@ -4,10 +4,22 @@ public class ErzeugerVerbraucherTest {
     public static void main(String[] args) {
         RingBuffer buffer = new RingBuffer("Puffer", 4);
         Producer producer = new Producer(buffer);
+        Producer producer2 = new Producer(buffer);
         Consumer consumer = new Consumer(buffer);
+        Consumer consumer2 = new Consumer(buffer);
+        Consumer consumer3 = new Consumer(buffer);
+        Consumer consumer4 = new Consumer(buffer);
+        Consumer consumer5 = new Consumer(buffer);
+        Consumer consumer6 = new Consumer(buffer);
         producer.start();
+        producer2.start();
         System.out.println("Erzeuger nimmt Arbeit auf");
         consumer.start();
+        consumer2.start();
+        consumer3.start();
+        consumer4.start();
+        consumer5.start();
+        consumer6.start();
         System.out.println("Verbraucher nimmt Arbeit auf");
     }
 }
